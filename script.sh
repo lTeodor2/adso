8820202ef2395358943b1982a404036656479e56
root <root@servidorexamen.malakor.sith>
2019-11-19 13:44:22 +0100

    Script console mod

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ let nom
 read -p "Tu nombre: " nom
 let ap
 read -p "Tu apellido: " ap
-var_ou=pilotos
+var_ou=guerreros
 var_dc1=malakor
 var_dc2=sith
 var_niud=10000
 var_ngid=10000
 var_pass=$nom
-echo	dn: uid=$nom,ou=$var_ou,cn=$var_dc1,cn=$var_dc2 >> user.ldif
+echo	dn: uid=$nom,ou=$var_ou,dc=$var_dc1,dc=$var_dc2 >> user.ldif
 echo	objectClass:inetOrgPerson >> user.ldif
 echo	objectClass:shadowAccount >> user.ldif
 echo	objectClass:posixAccount >> user.ldif
@@ -26,10 +26,11 @@ echo	shadowMin: 1 >> user.ldif
 echo	shadowWarning: 7 >> user.ldif
 echo	shadowInactive: 7 >> user.ldif
 echo	shadowLastChange: 0 >> user.ldif
-
+echo 	-e >> user.ldif
 echo	dn: cn=$nom,ou=$var_ou,dc=$var_dc1,dc=$var_dc2 >> user.ldif
 echo	objectClass: posixGroup >> user.ldif
 echo	cn: $nom >> user.ldif
 echo	gidNumber: $var_ngid >> user.ldif
 echo	memberUid: $nom >> user.ldif
 
+ldapadd -x -D cn="admin",dc=$var_dc1,dc=$var_dc2 -W -f user.ldif
